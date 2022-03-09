2fce4be094f1f307c4c41d077e0547038f36bd48
Daniel Resende <97549888+DanielResende23@users.noreply.github.com>
2022-03-08 22:28:49 -0300

    Add files via upload

---
+++ starbucks.js
@@ -0,0 +1,17 @@
+function imgSlider(anything){
+    document.querySelector('.starbucks').src = anything
+}
+
+function changeCircleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color
+
+}
+
+function toggleMenu()
+{
+    let menuToggle = document.querySelector('.toggle')
+    let navigation = document.querySelector('.navigation')
+    menuToggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
